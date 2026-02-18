a0f9bf684ed180e71c54e8483dce0e6d8a985a98
furkanturker <furkanturker.dev@gmail.com>
2026-02-19 00:25:56 +0300

    Mesaj gönderme işlemi yapıldı

--- ApiProjeKampi.WebUI/Controllers/MessageController.cs
+++ ApiProjeKampi.WebUI/Controllers/MessageController.cs
@@ -125,4 +125,23 @@ public class MessageController : Controller
         }
         return View(values);
     }
+
+    public PartialViewResult SendMessage()
+    {
+        return PartialView();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> SendMessage(CreateMessageDto createMessageDto)
+    {
+        var client = _httpClientFactory.CreateClient();
+        var jsonData = JsonConvert.SerializeObject(createMessageDto);
+        StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+        var responseMessage = await client.PostAsync("http://localhost:5083/api/Messages", stringContent);
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            return RedirectToAction("MessageList");
+        }
+        return View();
+    }
 }
